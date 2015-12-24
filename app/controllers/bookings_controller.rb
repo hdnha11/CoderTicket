db65c2a7f43b431911962270dad50dd79d08004a
Nha Hoang <hdnha11@gmail.com>
2015-12-25 00:17:31 +0700

    Add RSpec and refactor code

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,24 +7,14 @@ class BookingsController < ApplicationController
     end
 
     @booking = Booking.new(booking_params)
-    total = 0
-    total_quantity = 0
 
-    params[:booking][:tickets].each do |ticket|
-      quantity = ticket[:quantity].to_i
-      ticket_type = TicketType.find(ticket[:ticket_type_id])
-      ticket_type.max_quantity -= quantity
-      total += quantity * ticket_type.price
-      total_quantity += quantity
-
-      ticket_type.save!
-
-      @booking.tickets << Ticket.new(ticket_type: ticket_type, quantity: quantity)
+    begin
+      unless @booking.add_tickets(params[:booking][:tickets])
+        return redirect_to new_event_ticket_path, flash: {error: "Choose the quantity please!"}
+      end
+    rescue => ex
+      return redirect_to new_event_ticket_path, flash: {error: "Oops! #{ex}"}
     end
-
-    return redirect_to new_event_ticket_path, flash: {error: "Choose the quantity please!"} if total_quantity <= 0
-
-    @booking.total = total
 
     if @booking.save
       redirect_to @booking, flash: {success: "Booking successful!"}
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,29 @@ class Booking < ActiveRecord::Base
   has_many :tickets
 
   validates_presence_of :tickets
+
+  def add_tickets(tickets)
+    total = 0
+    total_quantity = 0
+
+    TicketType.transaction do
+      tickets.each do |ticket|
+        quantity = ticket[:quantity].to_i
+        if quantity > 0
+          ticket_type = TicketType.find(ticket[:ticket_type_id])
+          ticket_type.max_quantity -= quantity
+          total += quantity * ticket_type.price
+          total_quantity += quantity
+
+          ticket_type.save!
+
+          self.tickets << Ticket.new(ticket_type: ticket_type, quantity: quantity)
+        end
+      end
+    end
+
+    self.total = total
+
+    total_quantity > 0
+  end
 end
